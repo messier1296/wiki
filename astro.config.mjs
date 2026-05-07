06a3a13d26021ac8f2fbc611126733a311728048
messier1296 <hayato1048576@gmail.com>
2026-05-07 09:48:52 +0900

    Configure Astro for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://messier1296.github.io",
+	base: "/wiki",
+
 	markdown: {
 		remarkPlugins: [remarkMath],
 		rehypePlugins: [rehypeRaw, rehypeKatex],
